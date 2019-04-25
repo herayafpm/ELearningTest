5dae0764be5c1b32e92d4703d43e8138b730b436
raydragneel <49826694+raydragneel@users.noreply.github.com>
2019-04-25 07:47:59 +0700

    Initial commit

---
+++ app/src/main/java/com/heraya/e_learning/EmptyActivity.java
@@ -0,0 +1,18 @@
+package com.heraya.e_learning;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EmptyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_empty);
+    }
+    private void loadFragment (Fragment fr){
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+    }
+}
---
+++ app/src/main/java/com/heraya/e_learning/fragment/RegisterFragment.java
@@ -0,0 +1,6 @@
+package com.heraya.e_learning.fragment;
+import android.support.v4.app.Fragment;
+
+public class RegisterFragment extends Fragment {
+    private String nama = "Heraya";
+}
